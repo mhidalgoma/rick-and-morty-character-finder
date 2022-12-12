518d31661ec467a47c1800bbf23516d2bdd22c2e
Monica Hidalgo Martin <monicahidalgomartin@gmail.com>
2022-12-12 21:53:02 +0000

    finish function render characters

--- src/components/App.js
+++ src/components/App.js
@@ -4,22 +4,53 @@ import { useState, useEffect } from 'react';
 function App() {
   //VARIABLES ESTADO
   const [data, setData] = useState([]);
+
   //USE EFFECT
   useEffect(() => {
     // Dentro de useEffect llamamos a la API
     fetch('https://rickandmortyapi.com/api/character')
       .then((response) => response.json())
       .then((data) => {
-        setData(data.results);
+        const cleanData = data.results.map((character) => {
+          return {
+            id: character.id,
+            image: character.image,
+            name: character.name,
+            species: character.species,
+            planet: character.origin.name,
+            episodes: character.episode.length,
+            status: character.status,
+          };
+        });
+        setData(cleanData);
       });
   }, []);
 
   console.log(data);
 
   //FUNCIONES HANDLER
+  //FUNCIONES RENDER
+  const renderCharacters = () => {
+    const card = data.map((character) => {
+      return (
+        <li>
+          <img src={character.image} alt={`Image of ${character.name}`} />
+          <p>{character.name}</p>
+          <p>{character.species}</p>
+        </li>
+      );
+    });
+    return card;
+  };
+
   return (
     <div>
-      <h1>Hola Mundo</h1>
+      <header>
+        <h1>Rick y Morty</h1>
+      </header>
+      <main>
+        <ul>{renderCharacters()}</ul>
+      </main>
     </div>
   );
 }
